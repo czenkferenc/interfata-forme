9a4a877bc0f5d00028806cc716689e84206c1e05
Feri Czenk <feri.czenk@hotmail.com>
2020-06-04 10:29:49 +0300

    Cerc

---
+++ interfata forme/Cerc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfata_forme
+{
+    class Cerc : IForma2D
+    {
+        private double raza;
+
+        public Cerc(double r)
+        {
+            raza = r;
+        }
+
+        public string Denumire
+        {
+            get { return "Cerc"; }
+        }
+
+        public double Arie()
+        {
+            return Math.PI * raza * raza;
+        }
+
+        public double LungimeaFrontierei()
+        {
+            return 2 * Math.PI * raza;
+        }
+    }
+}
