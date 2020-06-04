eeb1dbefbcc5b926daa000bc7258bb8b587b1daa
Feri Czenk <feri.czenk@hotmail.com>
2020-06-04 10:15:47 +0300

    Patrat

---
+++ interfata forme/Patrat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfata_forme
+{
+    class Patrat : IForma2D
+    {
+        private double latura;
+
+        public Patrat(double l)
+        {
+            latura = l;
+        }
+
+        public double Arie()
+        {
+            return latura * latura;
+        }
+
+        public double LungimeaFrontierei()
+        {
+            return 4 * latura;
+        }
+
+        public string Denumire
+        {
+            get { return "Patrat"; }
+        }
+
+    }
+}
